590150d2bdceda8685d02686cad0d52a92e70a91
sulmi24 <a.sulmi24@gmail.com>
2023-03-21 16:25:01 +0200

    Solved : Assignment Done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  MesaVerde();
+  console.log(trees);
+  wildlife();
   return <h1>Colorado State Parks!</h1>;
 }
